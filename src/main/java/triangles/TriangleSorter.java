a90e8bccbf9f26d024b289fce29944af74e00f40
Tyler Gandy <tylergandy@ymail.com>
2020-11-22 22:59:03 -0500

    Second test done with passing results

--- src/main/java/triangles/TriangleSorter.java
+++ src/main/java/triangles/TriangleSorter.java
@@ -4,6 +4,8 @@ public class TriangleSorter {
     public String analyze(int sideA, int sideB, int sideC) {
         if(sideA == sideB && sideA == sideC)
             return "Equilateral";
+        else if(sideA == sideB || sideA == sideC || sideB == sideC)
+            return "Isosceles";
 
         return "";
     }
