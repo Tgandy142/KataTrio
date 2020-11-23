9deadce71a587ccbfae35e7c7ed329c2a2bfeaac
Tyler Gandy <tylergandy@ymail.com>
2020-11-22 22:52:01 -0500

    Initial Commit + First test done

--- src/main/java/triangles/TriangleSorter.java
+++ src/main/java/triangles/TriangleSorter.java
@@ -2,6 +2,9 @@ package triangles;
 
 public class TriangleSorter {
     public String analyze(int sideA, int sideB, int sideC) {
+        if(sideA == sideB && sideA == sideC)
+            return "Equilateral";
+
         return "";
     }
 }
