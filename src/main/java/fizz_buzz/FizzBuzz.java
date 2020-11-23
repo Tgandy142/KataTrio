8461afadbbe2b8609077882686c468c0ed0da6a8
Tyler Gandy <tylergandy@ymail.com>
2020-11-22 23:36:50 -0500

    Finished Tests with passing results, Refactored to allow for multiples of 3 or 5

--- src/main/java/fizz_buzz/FizzBuzz.java
+++ src/main/java/fizz_buzz/FizzBuzz.java
@@ -2,8 +2,10 @@ package fizz_buzz;
 
 public class FizzBuzz {
     public String say(Integer number) {
-        if(number == 3)
+        if(number % 3 == 0)
             return "Fizz";
+        else if(number % 5 == 0)
+            return "Buzz";
         return ""+number;
     }
 }
